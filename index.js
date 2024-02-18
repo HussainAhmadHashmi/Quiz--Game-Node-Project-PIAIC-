6843de36b9c16f7834dcd27c0e829caa2d67da34
Hussain Ahmad Faraz <fs5331775@gmail.com>
2024-02-18 10:38:09 +0500

    Quiz Project

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-import inquirer from "inquirer"
-import showBanner from "node-banner"
-
+import inquirer from "inquirer";
+import showBanner from "node-banner";
 (async () => {
     await showBanner('Quiz APP', 'Select you require transaction');
 })();
-
-
-let againQuizStart=[
+let againQuizStart = [
     {
         name: "again",
         type: "confirm",
-        message: "Do you want another Quiz? Prees 'y' "
+        message: "Do you want another Quiz"
     }
-]
-
+];
 const categoryQuestions = [
     {
         type: "list",
         name: "category",
-        choices: ["TypeScript", "HTML", "CSS", "Bootstrap5"],
+        choices: ["TypeScript", "HTML", "CSS"],
         message: "Select your preferred category:"
     }
 ];
-
-
-
-function getQuestionsForCategory(category: string) {
+function getQuestionsForCategory(category) {
     switch (category) {
         case "TypeScript":
             return [
@@ -59,8 +52,7 @@ function getQuestionsForCategory(category: string) {
                     choices: ["Server-side scripting.", " Database management.", "Adding static typing to JavaScript.", "Styling web pages."],
                     message: "Question 5: What is TypeScript primarily used for?"
                 }
-            ]
-
+            ];
         case "HTML":
             return [
                 {
@@ -93,8 +85,7 @@ function getQuestionsForCategory(category: string) {
                     choices: ["strong", "em", "u", "s"],
                     message: `Question 5: Which HTML tag is used to emphasize text strongly?`
                 }
-            ]
-
+            ];
         case "CSS":
             return [
                 {
@@ -128,91 +119,44 @@ function getQuestionsForCategory(category: string) {
                     message: `Question 5: Which CSS property is used for adding effects and animations?`
                 }
             ];
-
-        case "Bootstrap5":
-            return [
-                {
-                    type: "list",
-                    name: "Question1",
-                    choices: ["A programming language", " A front-end framework for web development", " A database management system", " A server-side scripting language"],
-                    message: `Question 1:  What is Bootstrap?`
-                },
-                {
-                    type: "list",
-                    name: "Question1",
-                    choices: [".navbar-fixed", ".navbar-expand", ".nav-responsive", "responsive-bar"],
-                    message: `Question 2:  Which class is used to create a responsive navigation bar in Bootstrap 5?`
-                },
-                {
-                    type: "list",
-                    name: "Question1",
-                    choices: [".d-none", ".hidden", ".invisible", ".hide"],
-                    message: `Question 3:   In Bootstrap 5, which utility class is used to hide an element visually, but keep it accessible for screen readers?`
-                },
-                {
-                    type: "list",
-                    name: "Question1",
-                    choices: ["Using tables", "Using flexbox", "Using the '.container' class", ". Using the '.row' and '.col' classes"],
-                    message: `Question 4:   How can you create a responsive grid system with Bootstrap 5?`
-                },
-                {
-                    type: "list",
-                    name: "Question1",
-                    choices: [".col-gap", ".gutter", ".column-space", ".col-spacing"],
-                    message: `Question 5:    Which class is used for adding spacing between Bootstrap grid columns?`
-                },
-                
-                
-            ];
         default:
             return [];
     }
 }
-
-
 async function AskQuestions() {
     const { category } = await inquirer.prompt(categoryQuestions);
     let condition = true;
     while (condition) {
         let Score = 0;
         let totalQuestion = 5;
-
         const questions = getQuestionsForCategory(category);
-
         const answers = await inquirer.prompt(questions);
-
-        const correctAnswers: Record<string, string[]> = {
+        const correctAnswers = {
             TypeScript: ["const", "function myFunction() {}", "To define a blueprint for a structure, allowing the creation of objects with specific shapes.", "By using a colon (:) followed by the desired type", "Adding static typing to JavaScript."],
             HTML: ["body", "a", "ol", "form", "strong"],
             CSS: ["color", "background-color", "tag", "position", "animation"]
         };
-
         Object.entries(answers).forEach(([question, answer]) => {
-            const userAnswer = answer as string; // Explicitly cast 'answer' to string
+            const userAnswer = answer; // Explicitly cast 'answer' to string
             if (correctAnswers[category]?.includes(userAnswer)) {
                 console.log(`Correct!`);
                 Score++;
-            } else {
+            }
+            else {
                 console.log(`Incorrect. The correct answer is: ${correctAnswers[category]?.join(", ")}`);
             }
         });
-        
-
-        console.log(`\n Your current score: ${Score}/${totalQuestion}\n`);
-        if (Score >=3) {
+        console.log(`Your current score: ${Score}/${totalQuestion}\n`);
+        if (Score >= 3) {
             console.log(`Congratulation! You are PASS this quiz`);
         }
-        else{
-            console.log(`You are FAIL, Will be next time `);
+        else {
+            console.log(`You are FAIL, Will be next time \n If you want quiz again Type 'y'`);
         }
-
-
         let { again } = await inquirer.prompt(againQuizStart);
         condition = again;
     }
 }
-
-
 setTimeout(() => {
-    AskQuestions()
+    AskQuestions();
 }, 2000);
